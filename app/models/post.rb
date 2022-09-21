979878c063a23a0f37b370a026547a66e5059e0e
Emma Cohanim <emmacohanim@gmail.com>
2022-09-21 08:33:48 -0700

    commit tree 88995de108ef8a76ca41aefaff48e8e51d567595

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    # validate :is_clickbait?
+
+    # def is_clickbait?
+    #     # unless title inlcudes one of the following, throw error
+    #     unless title.method
+    #         errors.add(:title, "not sufficiently clickbait-y")
+    #     end
+    # end
+
 end
